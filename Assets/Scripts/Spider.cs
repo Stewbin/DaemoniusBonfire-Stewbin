b38f84a1d23e32b58b0746f161190a2ed494cf38
christianmonfiston <109248449+christianmonfiston@users.noreply.github.com>
2024-02-06 23:37:10 -0500

    Trying add patrolling for spider

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Spider : MonoBehaviour
@@ -7,33 +8,45 @@ public class Spider : MonoBehaviour
     [SerializeField] public float spiderSpeed;
     [SerializeField] public GameObject spider;
     [SerializeField] public Rigidbody2D spiderBody;
+    public GameObject pointA;
+    public GameObject pointB;
+    public Animator anim;
+    public Transform currentPoint;
 
 
     void Start()
     {
         spiderBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isRunning", true);
     }
 
     void Update()
     {
 
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                print("Working"); 
-            }
-        }
 
-        SpiderPatrolling(); 
+        CheckCollision(); 
+         SpiderPatrolling(); 
         
     }
 
     public void SpiderPatrolling()
     {
-        spiderSpeed = -2.0f; 
+        spiderSpeed = -2.0f;
+
+        Vector2 point = currentPoint.position - transform.position; 
         spiderBody.velocity = new Vector2 (spiderSpeed, spiderBody.velocity.y);
     }
 
-    
+    public void CheckCollision()
+    {
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                print("Working");
+            }
+        }
+    }
 }
