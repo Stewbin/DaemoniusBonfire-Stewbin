9adca823533acea7221684df1740cf49547e41f3
christianmonfiston <109248449+christianmonfiston@users.noreply.github.com>
2024-02-06 23:07:47 -0500

    Still working on spider movememnts

--- Assets/Scripts/Spider.cs
+++ Assets/Scripts/Spider.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Spider : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public float spiderSpeed;
+    [SerializeField] public GameObject spider;
+    [SerializeField] public Rigidbody2D spiderBody;
+
+
     void Start()
     {
+        spiderBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                print("Working"); 
+            }
+        }
+
+        SpiderPatrolling(); 
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpiderPatrolling()
     {
-        
+        spiderSpeed = -2.0f; 
+        spiderBody.velocity = new Vector2 (spiderSpeed, spiderBody.velocity.y);
     }
+
+    
 }
