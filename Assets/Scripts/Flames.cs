3da3a38ed0cca6b4aa5f20bb9b818b031449231e
Theo <95878514+TheoW03@users.noreply.github.com>
2024-03-27 16:29:02 -0400

    h

--- Assets/Scripts/Flames.cs
+++ Assets/Scripts/Flames.cs
@@ -7,27 +7,19 @@ public class Flames : MonoBehaviour
     public int FlameThrowerLength;
     // Start is called before the first frame update
     public ParticleSystem Fire;
-
+    public PlayerStats PlayerStats;
     void Start()
     {
         Fire.transform.position = transform.position;
         Fire.Stop();
     }
-
-    // Update is called once per frame
     void Update()
     {
         Debug.DrawRay(transform.position, transform.right * 10, Color.green);
         if (Physics2D.Raycast(transform.position, transform.right, FlameThrowerLength).collider != null &&
                 Physics2D.Raycast(transform.position, transform.right, FlameThrowerLength).collider.gameObject.tag == "Player")
-        {
-
             Fire.Play();
-        }
         else
-        {
             Fire.Stop();
-
-        }
     }
 }
