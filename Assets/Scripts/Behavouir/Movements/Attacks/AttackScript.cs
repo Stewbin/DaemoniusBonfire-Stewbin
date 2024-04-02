0c271923c4e30b41c74ce4676205598f0c46554b
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-02 16:03:36 -0400

    this entire thing is either stupid or smart

--- Assets/Scripts/Behavouir/Movements/Attacks/AttackScript.cs
+++ Assets/Scripts/Behavouir/Movements/Attacks/AttackScript.cs
@@ -1,15 +1,16 @@
 
+using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
-    public AttackType attack;
-    public GameObject target;
-    void Start()
-    {
+	public AttackType attack;
+	public GameObject target;
+	void Start()
+	{
 
-    }
-    void Update()
-    {
+	}
+	void Update()
+	{
 
-    }
+	}
 
 }
--- Assets/Scripts/Behavouir/Movements/Attacks/AttackType.cs
+++ Assets/Scripts/Behavouir/Movements/Attacks/AttackType.cs
@@ -1,5 +1,7 @@
+using UnityEngine;
+
 public class AttackType : ScriptableObject
 {
-    public virtual void attack(GameObject gameObjects) { }
+	public virtual void attack(GameObject gameObjects) { }
 
 }
