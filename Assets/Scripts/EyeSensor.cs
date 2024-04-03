133315af51be32bddfe897ad9ac3fa5b3baa70b3
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-03 16:29:13 -0400

    attempting FOV

--- Assets/Scripts/EyeSensor.cs
+++ Assets/Scripts/EyeSensor.cs
@@ -1,14 +1,28 @@
+using System;
 using UnityEngine;
 public class EyeSensor : MonoBehaviour
 {
 	//shoots ray and tells you if player hing you want to attack is in site s
-	//
+	public GameObject Targets;
+	// public Ray 
 	private void Start()
 	{
-
 	}
 	private void Update()
 	{
+		// // int n = 0;
+		// if (GetComponent<SpriteRenderer>().flipX)
+		// 	n = -1;
+		// else
+		// 	n = 1;
+		// transform.LookAt(new Vector2(0,0));
+
+		// Debug.DrawRay(transform.position,, Color.green); //debugging purposes
+	}
+	public bool isNear()
+	{
+		// Debug.DrawRay(position, direction * 3.0f, Color.green); //debugging purposes
+		return false;
 
 	}
 
