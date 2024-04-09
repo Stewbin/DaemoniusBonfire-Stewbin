b2bc73eeb47f58aa5883f6582cc0e7b7c7174f74
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-09 13:51:12 -0400

    added a more stronger damage asset :3

---
+++ Assets/Scripts/DamageDealtStats.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public class DamageDealtStats : MonoBehaviour
+{
+    [Range(0, 100)]
+    public int Damage;
+}
---
+++ Assets/Scripts/ElkanHealth.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElkanHealth : MonoBehaviour
+{
+
+    public PlayerStats p; //over haul we create either a local script with the player health
+                          //or a scriptable object with player health >:] dwabtit
+    void Awake()
+    {
+        p.currentHealth = 100;
+    }
+    void Update()
+    {
+        if (p.currentHealth < 100)
+        {
+            Debug.Log("player dead");
+            p.currentHealth = 100;
+        }
+    }
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        int L = other.gameObject.layer;
+        if (L == LayerMask.NameToLayer("damageLayer"))
+        {
+            if (other.gameObject.GetComponent<DamageDealtStats>() == null) return;
+            p.currentHealth -= other.gameObject.GetComponent<DamageDealtStats>().Damage;
+            Debug.Log(p.currentHealth);
+        }
+
+    }
+}
