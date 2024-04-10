4eb3aca21ea918a5e080f7e51c23ca147ead20bf
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-10 11:34:39 -0400

    added stuff :]

--- Assets/Scripts/ElkanHealth.cs
+++ Assets/Scripts/ElkanHealth.cs
@@ -5,30 +5,31 @@ using UnityEngine;
 public class ElkanHealth : MonoBehaviour
 {
 
-    public PlayerStats p; //over haul we create either a local script with the player health
-                          //or a scriptable object with player health >:] dwabtit
-    void Awake()
-    {
-        p.currentHealth = 100;
-    }
-    void Update()
-    {
-        if (p.currentHealth < 100)
-        {
-            Debug.Log("player dead");
-            p.currentHealth = 100;
-        }
-    }
-    // Start is called before the first frame update
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        int L = other.gameObject.layer;
-        if (L == LayerMask.NameToLayer("damageLayer"))
-        {
-            if (other.gameObject.GetComponent<DamageDealtStats>() == null) return;
-            p.currentHealth -= other.gameObject.GetComponent<DamageDealtStats>().Damage;
-            Debug.Log(p.currentHealth);
-        }
+	public PlayerStats p; //over haul we create either a local script with the player health
+						  //or a scriptable object with player health >:] dwabtit
+	void Awake()
+	{
+		p.currentHealth = 100;
+	}
+	void Update()
+	{
+		if (p.currentHealth < 100)
+		{
+			Debug.Log("player dead");
+			// p.currentHealth = 100;
+			
+		}
+	}
+	// Start is called before the first frame update
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		int L = other.gameObject.layer;
+		if (L == LayerMask.NameToLayer("damageLayer"))
+		{
+			if (other.gameObject.GetComponent<DamageDealtStats>() == null) return;
+			p.currentHealth -= other.gameObject.GetComponent<DamageDealtStats>().Damage;
+			Debug.Log(p.currentHealth);
+		}
 
-    }
+	}
 }
