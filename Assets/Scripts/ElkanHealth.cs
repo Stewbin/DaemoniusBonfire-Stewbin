0791c8170c865d69d07a4637cee96d76580a4dae
Theo <95878514+TheoW03@users.noreply.github.com>
2024-04-17 15:22:27 -0400

    added damage indicator

--- Assets/Scripts/ElkanHealth.cs
+++ Assets/Scripts/ElkanHealth.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class ElkanHealth : MonoBehaviour
 {
-
 	public PlayerStats p; //over haul we create either a local script with the player health
-						  //or a scriptable object with player health >:] dwabtit
+
+	//or a scriptable object with player health >:] dwabtit
 	void Awake()
 	{
 		p.currentHealth = 100;
 	}
+
 	void Update()
 	{
 		if (p.currentHealth < 100)
 		{
 			Debug.Log("player dead");
 			// p.currentHealth = 100;
-			
+
 		}
 	}
+
 	// Start is called before the first frame update
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		int L = other.gameObject.layer;
 		if (L == LayerMask.NameToLayer("damageLayer"))
 		{
-			if (other.gameObject.GetComponent<DamageDealtStats>() == null) return;
+			if (other.gameObject.GetComponent<DamageDealtStats>() == null)
+				return;
 			p.currentHealth -= other.gameObject.GetComponent<DamageDealtStats>().Damage;
-			Debug.Log(p.currentHealth);
+			StartCoroutine(DamageIndicator());
 		}
+	}
 
+	IEnumerator DamageIndicator()
+	{
+		gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+		yield return new WaitForSeconds(0.5f);
+		gameObject.GetComponent<SpriteRenderer>().color = Color.white;
 	}
 }
